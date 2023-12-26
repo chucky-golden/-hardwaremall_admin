47e10e2e584dbd74cc981a477b51990b954456cc
chucky-golden <chukschibyke92@gmail.com>
2023-12-26 19:09:25 +0100

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -101,9 +101,8 @@ const findProductWithSlug = async (req, res) => {
 // send top 8 uploaded product
 const topproducts = async (req, res) => {
     try{
-        console.log('test')
         const products = await Product.find().sort({ countperimport: -1 }).limit(8)
-        console.log('f', products)
+        
         if(products !== null){
             let response = await axios.post('https://vendors-jpnc.onrender.com/users/topproducts', {
                 products: products
